80c8d5f2d9f600788bc97b65f745a8304c9599ed
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2023-07-07 21:53:59 -0300

    Created card struct
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

---
+++ blackjack.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("Hello world \n");
+
+
+
+}
--- cards.c
+++ cards.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#define CARDLENGTH 1
+
+/* typedef struct deck { */
+
+
+
+/* } */
+
+typedef struct card{
+//Nomenclature taken from: https://en.wikipedia.org/wiki/Standard_52-card_deck
+
+	/* short uint rank; */
+	int rank;
+
+	//Suit: (C)lubs ♣, (D)iamonds ♦, (H)earts ♥, (S)pades ♠
+	char suit[CARDLENGTH];
+	
+}card;
 
 void main()
 {
+	card cards;
+
+	cards.rank = 4;
+	strcpy(cards.suit, "f");
+
 	printf("Hello world \n");
+	printf("Nombre: %s \n", cards.suit);
+	printf("Nombre: %d \n", cards.rank);
 
 
 
