a9cf5a093c88e75f9adc42beb1272d74b3b01d40
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2023-07-09 18:03:00 -0300

    removed comments
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- cardArithmetic.c
+++ cardArithmetic.c
@@ -4,15 +4,10 @@
 
 uint sumCards(card *cards[], int amountOfCards)
 {
-	/* printf("%d\n", amountOfCards); */
-	/* printf("%d\n", (*cards)[0].rank); */
-
 	int sum = 0;
 	for (int card = 0; card < amountOfCards; card++) {
-		/* printf("%d\n", cards[card]->rank); */
 		sum += cards[card]->rank;
 	}
-	printf("%d\n", sum);
 
    return sum;
 }
